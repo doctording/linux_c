11a08f9cbd8bc014a930650c8c7bdf3ff0dad6b5
doctording <dingwenlong2014@163.com>
2016-08-16 16:17:01 +0800

    add thread

---
+++ thread/thread_eg.c
@@ -0,0 +1,26 @@
+#include <stdio.h>  
+#include <pthread.h>  
+
+void* thread( void *arg )  
+{  
+    printf( "This is a thread and arg = %d.\n", *(int*)arg);  
+    *(int*)arg = 0;  
+    return arg;  
+}  
+
+int main( int argc, char *argv[] )  
+{  
+    pthread_t th;  
+    int ret;  
+    int arg = 10;  
+    int *thread_ret = NULL;  
+    ret = pthread_create( &th, NULL, thread, &arg ); //创建线程，并传递参数 
+    if( ret != 0 ){  
+        printf( "Create thread error!\n");  
+        return -1;  
+    }  
+    printf( "This is the main process.\n" );  
+    pthread_join( th, (void**)&thread_ret ); // 以阻塞的方式等待thread指定的线程结束 线程间同步的操作
+    printf( "thread_ret = %d.\n", *thread_ret );  
+    return 0;  
+}  
---
+++ thread/thread_simultaneously.c
@@ -0,0 +1,61 @@
+/*
+单核时 线程的同时执行需要靠cpu在线程之间的快速切换来实现
+除了局部变量外 其他变量都将在一个进程中的所有线程之间共享
+*/
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *thread_function(void *arg);
+int run_now = 1;
+char message[] = "Hello World";
+
+int main() {
+    int res;
+    pthread_t a_thread;
+    void *thread_result;
+    int print_count1 = 0;
+
+    res = pthread_create(&a_thread, NULL, thread_function, (void *)message);
+    if (res != 0) {
+        perror("Thread creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    while(print_count1++ < 20) {
+        if (run_now == 1) {
+            printf("1");
+            run_now = 2;
+        }
+        else {
+            sleep(1);
+        }
+    }
+
+    printf("\nWaiting for thread to finish...\n");
+    res = pthread_join(a_thread, &thread_result);
+    if (res != 0) {
+        perror("Thread join failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Thread joined\n");
+    exit(EXIT_SUCCESS);
+}
+
+void *thread_function(void *arg) {
+    int print_count2 = 0;
+
+    while(print_count2++ < 20) {
+        if (run_now == 2) {
+            printf("2");
+            run_now = 1;
+        }
+        else {
+            sleep(1);
+        }
+    }
+
+    sleep(3);
+}
+
